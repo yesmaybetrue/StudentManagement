9e72be64728ce6296db8773037cd88a15d9d9051
yesmaybetrue <80052298+yesmaybetrue@users.noreply.github.com>
2022-09-12 09:52:32 +0530

    Add project files.

---
+++ StudentManagement/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using  MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace StudentManagement.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Student
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [BsonElement("graduated")]
+        public bool Isgraduated { get; set; }
+
+        [BsonElement("courses")]
+        public string[]? Courses { get; set; }
+
+        [BsonElement("gender")]
+        public string Gender { get; set; } = string.Empty;
+
+        [BsonElement("age")]
+        public int Age { get; set; }
+    }
+}
